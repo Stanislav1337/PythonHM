00602c37d829c5d6186f15b92a01c21adfdad6aa
Stanislav <stasgrisha2@yandex.ru>
2023-02-12 19:49:27 +0300

    HM5 changed

--- HM5/Task2.py
+++ HM5/Task2.py
@@ -2,11 +2,11 @@
 # Из всех арифметических операций допускаются только +1 и -1. 
 # Также нельзя использовать циклы.
 def sum_AB (a, b):
-    if b == 0:
-        if a == 0:
+    if a == 0:
+        if b == 0:
             return 0
-        return sum_AB(a - 1, b) + 1
-    return sum_AB(a, b - 1) + 1
+        return sum_AB(a, b - 1) + 1
+    return sum_AB(a - 1, b) + 1
 
 
 n1 = int(input("введи 1-е число: "))
